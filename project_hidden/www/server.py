9442b7d50685bd3da1377a809b1fa4034c84b73d
Krit Karan <kritkaran94@users.noreply.github.com>
2016-04-08 16:55:46 +0530

    Added files via upload

---
+++ project_hidden/www/server.py
@@ -0,0 +1,41 @@
+from email.MIMEMultipart import MIMEMultipart
+from email.MIMEText import MIMEText
+import smtplib
+
+import flask
+
+app = flask.Flask(__name__)
+app.debug = True
+app.secret_key = 'iaeuwefLRN7DPU5vXAjr4gdGCdCvPY8BdjyFNHTzE8PfADcC'
+
+gmail_user = 'kritkaran21@gmail.com'
+gmail_pwd = 'satpalsingh'
+
+@app.route('/')
+def index():
+    return flask.render_template('index.html')
+
+@app.route('/contact', methods=['POST'])
+def contact():
+   text = flask.request.form['message']
+
+   msg = MIMEMultipart()
+
+   msg['From'] = gmail_user
+   msg['To'] = gmail_user
+   msg['Subject'] = 'PGP Credibility'
+
+   msg.attach(MIMEText(text))
+
+   smtp = smtplib.SMTP('smtp.gmail.com', 587)
+   smtp.ehlo()
+   smtp.starttls()
+   smtp.ehlo()
+   smtp.login(gmail_user, gmail_pwd)
+   smtp.sendmail(gmail_user, gmail_user, msg.as_string())
+   smtp.quit()
+   smtp.close()
+   return 'Message has been successfuly sent!'
+
+if __name__ == '__main__':
+    app.run()
